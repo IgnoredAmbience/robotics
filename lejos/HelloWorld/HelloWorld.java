99e27a451e06baf78357ccecf5346af79ce11ed1
Thomas Wood <grand.edgemaster@gmail.com>
2012-02-16 12:35:29 +0000

    I dunno what changed here

--- lejos/HelloWorld/HelloWorld.java
+++ lejos/HelloWorld/HelloWorld.java
@@ -1,8 +1,13 @@
 import lejos.nxt.Button;
+import lejos.nxt.Motor;
 
 public class HelloWorld {
   public static void main (String[] args) {
     System.out.println("Hello World");
+    
+    Motor.A.forward();
+    Motor.B.forward();
+    
     Button.waitForAnyPress();
   }
 }
