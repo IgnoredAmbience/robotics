efe9a4838cd4f95d83285571a569f466b66bb817
Thomas Wood <grand.edgemaster@gmail.com>
2012-02-28 21:15:33 +0000

    Add particle scatter of tut5

--- lejos/tut5Probability/src/navigation/particles/ParticleSet.java
+++ lejos/tut5Probability/src/navigation/particles/ParticleSet.java
@@ -1,16 +1,56 @@
 package navigation.particles;
 
+import interfaces.Drawable;
 import java.util.ArrayList;
 
-public class ParticleSet extends ArrayList<Particle> {
+import lejos.robotics.navigation.Move;
+import lejos.robotics.navigation.MoveListener;
+import lejos.robotics.navigation.MoveProvider;
+
+
+public class ParticleSet extends ArrayList<Particle> implements Drawable, MoveListener {
 	private final static int MAX_SIZE = 100;
+	private final static float unitsPerPixel = 30;
+
 	
-	public ParticleSet () {
+	public ParticleSet() {
 		super(MAX_SIZE);
+		for(int i = 0; i < MAX_SIZE; i++) {
+			add(new Particle());
+		}
 	}	
 	
 	public float getWeight() {
 		return 1/this.size();
+	}
+	
+	public void moveUpdate(float distance) {
+		for(Particle p : this) {
+			p.moveUpdate(distance);
+		}
+	}
+	
+	public void rotateUpdate(float angle) {
+		for(Particle p : this) {
+			p.rotateUpdate(angle);
+		}
+	}
 
+	@Override
+	public void drawToLCD() {
+		for(Particle p : this) {
+			p.drawToLCD();
+		}
+	}
+
+	@Override
+	public void moveStarted(Move event, MoveProvider mp) {}
+
+	@Override
+	public void moveStopped(Move event, MoveProvider mp) {
+		for(Particle p : this) {
+			p.updatePose(event);
+		}
+		drawToLCD();
 	}
 }
--- lejos/tut5Probability/src/tasks/PositionPlotter.java
+++ lejos/tut5Probability/src/tasks/PositionPlotter.java
@@ -1,12 +1,10 @@
 package tasks;
 
 import lejos.nxt.LCD;
-import lejos.robotics.navigation.Pose;
 import robot.Robot;
 
 public class PositionPlotter extends Thread {
 	private Robot robot;
-	private float unitsPerPixel = 30;
 	
 	public PositionPlotter(Robot r) {
 		LCD.clear();
@@ -16,8 +14,7 @@ public class PositionPlotter extends Thread {
 	@Override
 	public void run() {
 		while(true) {
-			Pose p = robot.getCurrentPose();
-			LCD.setPixel((int) (p.getX()/unitsPerPixel), (int) (p.getY()/unitsPerPixel), 1);
+			robot.getCurrentPose().drawToLCD();
 			try {
 				sleep(10);
 			} catch (InterruptedException e) {}
